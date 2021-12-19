d544d140581c0e4a5a5794f99c1758a98d9049e7
Severyn12 <severyn.peleshko@gmail.com>
2021-12-19 13:25:46 +0200

    fixed some bugs

--- src/test/java/ua/edu/ucu/collections/LinkedListTest.java
+++ src/test/java/ua/edu/ucu/collections/LinkedListTest.java
@@ -12,10 +12,10 @@ public class LinkedListTest {
     @Test
     public void addTest(){
         ImmutableLinkedList nwLinkedList = (ImmutableLinkedList) linkedList.add(12);
-        assertEquals(nwLinkedList.getNode_amount(),1);
+        assertEquals(nwLinkedList.getNodeAmount(),1);
 
         ImmutableLinkedList nwLinkedList_2 = (ImmutableLinkedList) nwLinkedList.addAll(new Integer[] {124,23,45});
-        assertEquals(nwLinkedList_2.getNode_amount(),4);
+        assertEquals(nwLinkedList_2.getNodeAmount(),4);
 
         ImmutableLinkedList nwLinkedList_3 = (ImmutableLinkedList) nwLinkedList_2.add(2,70);
         assertEquals(nwLinkedList_3.toString(),"ImmutableLinkedList{" +
@@ -26,12 +26,12 @@ public class LinkedListTest {
         assertEquals(nwLinkedList_4.toString(),"ImmutableLinkedList{start_node=Node{data=70, next=null}}");
 
         ImmutableLinkedList nwLinkedList_5 =  new ImmutableLinkedList(new Integer[] {1,2,3});
-        assertEquals(nwLinkedList_5.getNode_amount(),3);
+        assertEquals(nwLinkedList_5.getNodeAmount(),3);
 
         ImmutableLinkedList nwLinkedList_6 = (ImmutableLinkedList) nwLinkedList_5.addAll(2,new Integer[] {124,56});
         assertEquals(nwLinkedList_6.toString(),"ImmutableLinkedList{start_node=Node{data=1, next=Node{data=2, " +
                 "next=Node{data=124, next=Node{data=56, next=Node{data=3, next=null}}}}}}");
-        assertEquals(nwLinkedList_6.getNode_amount(),5);
+        assertEquals(nwLinkedList_6.getNodeAmount(),5);
 
         ImmutableLinkedList nwLinkedList_7 =
                 (ImmutableLinkedList) new ImmutableLinkedList().addAll(10,new Integer[]{70});
@@ -48,7 +48,7 @@ public class LinkedListTest {
                 "next=Node{data=23, next=null}}}");
         ImmutableLinkedList nwLinkedList_4 = (ImmutableLinkedList) nwLinkedList_3.set(0,100);
         assertEquals(nwLinkedList_4.get(0),100);
-        assertEquals(nwLinkedList_4.getNode_amount(),2);
+        assertEquals(nwLinkedList_4.getNodeAmount(),2);
         assertEquals(nwLinkedList_2.indexOf(23),1);
         ImmutableLinkedList nwLinkedList_5 = (ImmutableLinkedList) nwLinkedList_2.clear();
         assertEquals(nwLinkedList_5.size(),0);
